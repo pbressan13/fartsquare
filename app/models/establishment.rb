6bafe51ff19e7cbc8927ac0e91abfd2184fc32fd
Rubem Dornas <rapdornas@gmail.com>
2021-11-16 23:38:32 -0300

    Finalizando mais uma etapa de seed, relativo à disponibilidade

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -1,4 +1,27 @@
 class Establishment < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+  serialize :availability
+
+  def parse_date(string_date)
+    string_date.split(": ")[1].split(" –")
+  end
+
+  def open?
+    today_times = nil
+    self.availability.each do |date|
+      today_times = date if date.include?(Time.now.strftime("%A"))
+    end
+    if today_times.blank?
+      return false
+    elsif today_times.include?("Closed")
+      return false
+    elsif today_times.include?("24 hours")
+      return true
+    else
+      opening = Chronic.parse("this #{parse_date(today_times)[0]}")
+      closing = Chronic.parse("this #{parse_date(today_times)[1]}")
+      return Time.now > opening && Time.now < closing
+    end
+  end
 end
--- db/migrate/20211114075717_add_new_columns_to_establishments.rb
+++ db/migrate/20211114075717_add_new_columns_to_establishments.rb
@@ -5,5 +5,7 @@ class AddNewColumnsToEstablishments < ActiveRecord::Migration[6.1]
     add_column :establishments, :lat, :decimal
     add_column :establishments, :lng, :decimal
     add_column :establishments, :google_id, :string
+    add_column :establishments, :availability, :jsonb, default: "{}"
+    add_column :establishments, :business_status, :string
   end
 end
