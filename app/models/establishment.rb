a992d26650459bd929f0a55fc7ae4a58285452eb
pbressan13 <49291866+pbressan13@users.noreply.github.com>
2021-11-25 17:18:59 -0300

    Update establishment.rb

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -6,7 +6,6 @@ class Establishment < ApplicationRecord
   searchkick
   serialize :types
 
-
   def parse_date(string_date)
     string_date.split(': ')[1].split(' –')
   end
@@ -25,7 +24,6 @@ class Establishment < ApplicationRecord
     today_times = fetch_today_times
     return false if today_times.blank? || today_times.include?("Closed")
     return true if today_times.include?("24 hours")
-
     opening = Chronic.parse("this #{parse_date(today_times).first}")
     closing = Chronic.parse("this #{parse_date(today_times).last}")
     return Time.now > opening && Time.now < closing
