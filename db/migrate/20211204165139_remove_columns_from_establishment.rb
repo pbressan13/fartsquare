2f798081472df5b660fdef88b6f1357288c9bf02
Pedro Bressan <pbressan13@gmail.com>
2021-12-06 21:43:49 -0300

    Added SEO and meta info

--- db/migrate/20211204165139_remove_columns_from_establishment.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnsFromEstablishment < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :establishments, :avaliable_now?, :string
-    remove_column :establishments, :available_now?, :string
-  end
-end
