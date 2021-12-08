f8f05af6832f3c36bb19a4af16814c4f1d9bdcb8
Rubem Dornas <rapdornas@gmail.com>
2021-12-07 21:44:38 -0300

    Retirando print

--- app/jobs/update_establishment_job.rb
+++ app/jobs/update_establishment_job.rb
@@ -4,7 +4,6 @@ class UpdateEstablishmentJob
 
   def duration_from_current_user(longitude, latitude, id)
     user = User.find(id)
-    p user
     url = "https://api.mapbox.com/directions/v5/mapbox/driving/#{user.longitude}%2C#{user.latitude}%3B#{longitude}%2C#{latitude}?alternatives=false&annotations=distance%2Cduration&continue_straight=false&geometries=geojson&overview=full&steps=false&access_token=#{ENV['MAPBOX_API_KEY']}"
     read_url = URI.parse(url).read
     request = JSON.parse(read_url).deep_symbolize_keys!
